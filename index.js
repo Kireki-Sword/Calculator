88c620fee069332cbb0e9af0b02e02f76a5587ec
Kireki-Sword <286178854+Kireki-Sword@users.noreply.github.com>
2026-06-18 22:52:59 +0000

    final edit

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const displayValue = document.getElementById('display')
 
 function appendToDisplay(input){
     displayValue.value += input;
+    displayValue.scrollLeft = displayValue.scrollWidth;
 }
 function clearDisplay(){
     displayValue.value = '';
 }
 function calculate(){
     try {
-        displayValue.value = eval(displayValue.value)
+        displayValue.value = eval(displayValue.value).toFixed(2)
 
     }
 
